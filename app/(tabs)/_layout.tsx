dc10192e6f45d1b227dece51aed48aaa995b82d2
Felipe <felipe@gooseinsurance.com>
2025-09-05 14:15:37 -0700

    Removed Details tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,13 +34,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="details"
-        options={{
-          title: "Details",
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="note" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="favourites"
         options={{
           title: "Favourites",
--- app/(tabs)/details.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TabTwoScreen() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "red", justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello Details</Text>
-    </View>
-  );
-}
