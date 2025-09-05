c104eef15e4f43519e9dcabe64524b12263ff903
Felipe <felipe@gooseinsurance.com>
2025-09-05 11:36:06 -0700

    initial commit

---
+++ app/(tabs)/details.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "red", justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello Details</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/favourites.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "blue", justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello Favourites</Text>
+    </View>
+  );
+}
